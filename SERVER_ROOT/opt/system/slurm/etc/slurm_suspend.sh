1a7b0cdba6bb6ef24cb5ab00d96b5f082d862824
Jason Wells <98784673+jrwellshpc@users.noreply.github.com>
2025-04-23 13:01:36 -0400

    Update and rename suspend.sh to slurm_suspend.sh

--- SERVER_ROOT/opt/system/slurm/etc/slurm_suspend.sh
+++ SERVER_ROOT/opt/system/slurm/etc/slurm_suspend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Example SuspendProgram
-echo "`date` Suspend invoked $0 $*" >> /var/log/power_save.log
-hosts=`scontrol show hostnames $1`
+echo $(date) "Resume invoked $0 $*" >> /var/log/power_save.log
+hosts=$(scontrol show hostnames $1)
 for host in $hosts
 do
    echo "Suspending " $host >> /var/log/power_save.log
