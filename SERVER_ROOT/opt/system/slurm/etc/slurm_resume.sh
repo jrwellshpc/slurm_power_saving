c18f2b43159f11cf1ff825517299b126244d41a6
Jason Wells <98784673+jrwellshpc@users.noreply.github.com>
2025-04-23 12:59:56 -0400

    Update and rename resume.sh to slurm_resume.sh
    Updated name to specify that this is the one called by slurm, and for the echo and hosts lines to work better.

--- SERVER_ROOT/opt/system/slurm/etc/slurm_resume.sh
+++ SERVER_ROOT/opt/system/slurm/etc/slurm_resume.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Example ResumeProgram
-echo "`date` Resume invoked $0 $*" >> /var/log/power_save.log
-hosts=`scontrol show hostnames $1`
+echo $(date) "Resume invoked $0 $*" >>  /var/log/power_save.log
+hosts=$(scontrol show hostnames $1)
 for host in $hosts
 do
    echo "Starting up " $host >> /var/log/power_save.log
