cb021957b5b26be8dbab8c4ec744f6e0f81ab0c0
Jason Wells <98784673+jrwellshpc@users.noreply.github.com>
2025-11-13 17:17:14 -0500

    Update cron_file.sh

--- SERVER_ROOT/opt/system/slurm/etc/cron_file.sh
+++ SERVER_ROOT/opt/system/slurm/etc/cron_file.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 # This is a cron file that should run daily and do maintenance tasks, like ensure your environment stays up to date.
+# The below will print a list of the suspended nodes:
+sinfo | grep idle~ | awk '{print $6}' | xargs -I {} scontrol show hostname {} 2>/dev/null
+
+# from there you might consider adding "| xargs -I {} ipmitool flagsGoHere {} chassis power on 2>/dev/null" to the end. 
+# That would then boot the nodes, allowing the configuration steps to complete, and then your existing timeout would 
+# suspend the node. 
